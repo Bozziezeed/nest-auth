77886e56a01910db8c3f549db6cd1b8fbd1ac1b3
Boss Mindbot <netiwit.mhtech@gmail.com>
2024-09-18 22:50:08 +0700

    [fix]: clean code

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,32 +6,6 @@ import { User } from './user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
 
-// @Module({
-//   imports: [
-//     ConfigModule.forRoot({
-//       envFilePath: '.development.env',
-//       isGlobal: true,
-//     }),
-//     TypeOrmModule.forRoot({
-//       type: 'mysql',
-//       host: 'localhost',
-//       port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
-//       username: process.env.DATABASE_USER,
-//       password: process.env.DATABASE_PASSWORD,
-//       database: process.env.DATABASE_NAME,
-//       entities: [User],
-//       synchronize: true,
-//     }),
-//     TypeOrmModule.forFeature([User]),
-//     JwtModule.register({
-//       secret: process.env.JWT_SECRETKEY,
-//       signOptions: { expiresIn: '1d' },
-//     }),
-//   ],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
-// export class AppModule {}
 @Module({
   imports: [
     ConfigModule.forRoot({
